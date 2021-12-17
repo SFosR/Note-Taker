283b8be776a3f45402e3ae693a699be4579610f1
Sheri Foster <fostr.sheri@gmail.com>
2021-12-17 12:17:48 -0500

    "port in caps"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/apiroutes.js');
 const html = require('./routes/htmlroutes.js');
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
